1edb300d8c1e2b73d3c4dd81cb4171548a57baae
lenjrg <jjleon@students.pccc.edu>
2025-10-20 12:13:53 -0400

    lab4

---
+++ labs/lab4/challenge_lab4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "―――――――――――――――――――――"
+figlet "System"
+echo "$LINE"
+echo -e "DATE UTC:\t $(date --rfc-3339='seconds')" 
+echo -e "HOSTNAME:\t $(whoami)"
+echo -e "UPTIME:\t 	 $(uptime --pretty)"
+echo -e "USER:\t	 $USER"
+figlet "MEMORY"
+free -h
+figlet DISK
+df -h /dev/sda1
+echo "―――――――――――――――――――――"
